c19baa23cd7bae673f5e52baf12c22ac6f5b0f27
Connoropolous <connorturland@gmail.com>
2013-09-20 17:52:06 -0400

    Update App.js
    removed extra code, that didn't work

--- javascripts/App.js
+++ javascripts/App.js
@@ -512,14 +512,14 @@ window.fbAsyncInit = function() {
       // (1) JavaScript created popup windows are blocked by most browsers unless they 
       // result from direct interaction from people using the app (such as a mouse click)
       // (2) it is a bad experience to be continually prompted to login upon page load.
-      FB.login(function(response){}, {scope: 'user_groups'});
+      FB.login();
     } else {
       // In this case, the person is not logged into Facebook, so we call the login() 
       // function to prompt them to do so. Note that at this stage there is no indication
       // of whether they are logged into the app. If they aren't then they'll see the Login
       // dialog right after they log in to Facebook. 
       // The same caveats as above apply to the FB.login() call here.
-      FB.login(function(response){}, {scope: 'user_groups'});
+      FB.login();
     }
   });
 };
